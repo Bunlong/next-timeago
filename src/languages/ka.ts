d45e49933b777aee7909143f258b7d005b8b7767
Bunlong <bunlong.van@gmail.com>
2022-09-04 11:56:00 +0700

    Define Georgian language

--- src/languages/ka.ts
+++ src/languages/ka.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Georgian ( ka ) */
+import type { L10nsStrings } from '../model';
 
-// English (Template)
 const strings: L10nsStrings = {
   prefixAgo: null,
   prefixFromNow: null,
