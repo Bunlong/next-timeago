e10c489a0394b353f87e3adea4cbfd65d2fc7963
Bunlong <bunlong.van@gmail.com>
2022-09-04 11:57:04 +0700

    Define Georgian shortened language

--- src/languages/ka-short.ts
+++ src/languages/ka-short.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Georgian shortened ( ka-short ) */
+import type { L10nsStrings } from '../model';
 
-// English shortened
 const strings: L10nsStrings = {
   prefixAgo: null,
   prefixFromNow: null,
