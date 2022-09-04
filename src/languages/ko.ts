b025159a57a1d5b9a3341dff223b3c30701ecc11
Bunlong <bunlong.van@gmail.com>
2022-09-04 11:58:08 +0700

    Define Korean language

--- src/languages/ko.ts
+++ src/languages/ko.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Korean ( ko ) */
+import type { L10nsStrings } from '../model';
 
-// Korean
 const strings: L10nsStrings = {
   prefixAgo: null,
   prefixFromNow: null,
--- src/useTimeAgo.tsx
+++ src/useTimeAgo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Props, MINUTE, HOUR, DAY, WEEK, MONTH, YEAR, Unit } from './model';
 import format from './format';
 import formatLocale from './formatLocale';
-import langStrings from './languages/ka';
+import langStrings from './languages/ko';
 
 function useTimeAgoComponent() {
   const TimeAgoComponent = ({
