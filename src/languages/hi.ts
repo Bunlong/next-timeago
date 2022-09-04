edc3fba6ae2d757b7992b69a1f770f5cfb214b7a
Bunlong <bunlong.van@gmail.com>
2022-09-04 11:38:29 +0700

    Define Hindi language

--- src/languages/hi.ts
+++ src/languages/hi.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Hindi ( hi ) */
+import type { L10nsStrings } from '../model';
 
-// Hindi (Template)
 const strings: L10nsStrings = {
   prefixAgo: null,
   prefixFromNow: 'अब से',
