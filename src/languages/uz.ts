d217537ba055d846a97820f61b8eec7e5ac0f6c8
Bunlong <bunlong.van@gmail.com>
2022-09-04 16:33:31 +0700

    Define Uzbek language

--- src/languages/uz.ts
+++ src/languages/uz.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Uzbek ( uz ) */
+import type { L10nsStrings } from '../model';
 
-// Uzbek
 const strings: L10nsStrings = {
   prefixAgo: null,
   prefixFromNow: 'keyin',
--- src/useTimeAgo.tsx
+++ src/useTimeAgo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Props, MINUTE, HOUR, DAY, WEEK, MONTH, YEAR, Unit } from './model';
 import format from './format';
 import formatLocale from './formatLocale';
-import langStrings from './languages/uk';
+import langStrings from './languages/uz';
 
 function useTimeAgoComponent() {
   const TimeAgoComponent = ({
