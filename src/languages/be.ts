193ed9e98ff6e3bd14a8a47e175dfd95d0bd1638
Bunlong <bunlong.van@gmail.com>
2022-09-04 10:49:16 +0700

    Define Belarusian language

--- src/languages/be.ts
+++ src/languages/be.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Belarusian ( be ) */
+import type { L10nsStrings } from '../model';
 
-// Belarusian
 function numpf(n: number, f: string, s: string, t: string): string {
   // f - 1, 21, 31, ...
   // s - 2-4, 22-24, 32-34 ...
