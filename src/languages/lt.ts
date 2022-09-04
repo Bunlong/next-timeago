a49c03c931bbbaaf21c2ad908f1224a6b4ebd97c
Bunlong <bunlong.van@gmail.com>
2022-09-04 12:00:08 +0700

    Define Lithuanian language

--- src/languages/lt.ts
+++ src/languages/lt.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Lithuanian ( lt ) */
+import type { L10nsStrings } from '../model';
 
-// Lithuanian
 const strings: L10nsStrings = {
   prefixAgo: 'prieš',
   prefixFromNow: null,
