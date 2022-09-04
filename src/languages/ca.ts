60d418d1d70a93482bcda6461bf0bf81509707cb
Bunlong <bunlong.van@gmail.com>
2022-09-04 10:55:32 +0700

    Define Bosnian language

--- src/languages/ca.ts
+++ src/languages/ca.ts
@@ -1,7 +1,6 @@
-/* @flow */
-import type { L10nsStrings } from '../formatters/buildFormatter';
+/* Catalan ( ca ) */
+import type { L10nsStrings } from '../model';
 
-// Catalan
 const strings: L10nsStrings = {
   prefixAgo: 'fa',
   prefixFromNow: "d'aqui a",
